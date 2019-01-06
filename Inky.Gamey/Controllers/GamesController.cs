da63e715fe6205551c78f3c6f4f20bb93e4d050f
Pete Whitby <petejw@theswamp.co.uk>
2019-01-06 17:35:01 +0000

    ...

--- Inky.Gamey/Controllers/GamesController.cs
+++ Inky.Gamey/Controllers/GamesController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Inky.Gamey.Data;
-using Inky.Gamey.Models;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 
@@ -101,8 +96,6 @@ namespace Inky.Gamey.Controllers
         }
 
         // POST: Games/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,Title")] Game game)
@@ -121,7 +114,7 @@ namespace Inky.Gamey.Controllers
                     return Unauthorized();
                 }
 
-
+                // Update game details
                 try
                 {
                     _context.Update(game);
@@ -142,6 +135,7 @@ namespace Inky.Gamey.Controllers
 
                 return RedirectToAction(nameof(Index));
             }
+
             return View(game);
         }
 
